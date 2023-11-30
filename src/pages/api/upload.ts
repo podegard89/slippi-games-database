f9eed7265a70999de267ab29fda6b79796801cc0
Pearce Odegard <pearceodegard@gmail.com>
2023-11-30 13:24:54 -0600

    Moved upload endpoint to external API, added enctype to form, minor clean up

--- src/pages/api/upload.ts
+++
@@ -1,7 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const POST: APIRoute = async ({ request, redirect }) => {
-  const data = await request.formData();
-  console.log(data);
-  return redirect("/dashboard");
-};
