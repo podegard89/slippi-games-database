25eb694b574b9e145af8052de0afc2a0eccc31b8
Pearce Odegard <pearceodegard@gmail.com>
2023-12-07 14:43:19 -0600

    Added env types, updated index page

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly SUPABASE_URL: string;
   readonly SUPABASE_ANON_KEY: string;
+  readonly SLP_UPLOAD_SERVICE_API_KEY: string;
+  readonly SLP_UPLOAD_API_ENDPOINT: string;
 }
 
 interface ImportMeta {
